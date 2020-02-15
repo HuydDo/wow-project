d032745dd8c5241f2e2c625f3835ad07214d494a
HuydDo <huydinhdo@yahoo.com>
2020-02-15 14:12:02 -0700

    refactor index.html, index.js

--- js-frontend/src/index.js
+++ js-frontend/src/index.js
@@ -1,38 +1,20 @@
 
-function getSelectVal(){
-  
-  let selectElem = document.getElementById("myGender");
-  console.log(selectElem)
-  document.getElementById("gender").value = selectElem.options[selectElem.selectedIndex].text;
-  // let pElem = document.getElementById('gender')
-  // selectElm.addEventListener('change', function() {
-  //   let index = selectElem.selectedIndex;
-  //   pElem.innerHTML = index
-  // })
-}
 const genders = ["Male","Female"]
 const races = ["Human","Orc","Dwarf","Night Elf","Undead","Tauren","Gnome","Troll","Goblin","Blood Elf","Draenei","Worgen","Pandaren","Nightborne","Highmountain Tauren","Void Elf","Lightforged Draenei","Zandalari Troll","Kul Tiran","Dark Iron Dwarf","Vulpera","Mag'har Orc","Mechagnome"]
-const character_classes = []
+const classes = ["Warrior","Paladin","Hunter","Rogue","Priest","Death Knight","Shaman","Mage","Warlock","Monk","Druid", "Demon Hunter"]
 
+function renderDropdown(tagId,arr) {
+  let div = document.querySelector(tagId),
+  frag = document.createDocumentFragment(),
+  select = document.createElement("select")
 
+  arr.forEach(e => select.options.add(new Option(e)))
+  frag.appendChild(select)
+  div.appendChild(frag)
+}
 
-let divGender = document.querySelector("#myGender"),
-    fragGender = document.createDocumentFragment(),
-    selectGender = document.createElement("select")
-    divRace = document.querySelector("#myRace"),
-    fragRace = document.createDocumentFragment(),
-    selectRace = document.createElement("select");
-
-    genders.forEach(gender => selectGender.options.add(new Option(gender)))
-    races.forEach(race => selectRace.options.add(new Option(race)))
-  
-    // select.options.add(new Option("Male"))
-    // select.options.add(new Option("Female"))
-    
-    fragGender.appendChild(selectGender);
-    divGender.appendChild(fragGender);
-
-    fragRace.appendChild(selectRace);
-    divRace.appendChild(fragRace);
+renderDropdown("#myGender", genders)
+renderDropdown("#myRace", races)
+renderDropdown("#myClass", classes)
 
 const app = new App()
