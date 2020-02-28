8d3e88326f180ce38066ca17ca711bb0bc0796f1
HuydDo <huydinhdo@yahoo.com>
2020-02-28 00:12:54 -0700

    Update character.js

--- js-frontend/src/components/character.js
+++ js-frontend/src/components/character.js
@@ -12,23 +12,9 @@ class Character{
   //   return `<li>${this.gender} ${this.name} ${this.race} ${this.character_class}</li>`
   // }
 
-
   renderLi(){
-    // let playerCharacters = []
-    
-    // this.characters.forEach(c => {
-      // playerCharacters.push(`<li>Character: ${c.gender} ${c.name} ${c.race} ${c.character_class} </li>`)
-      // playerCharacters.push(`<li>${c.name} - Race: ${c.race} Class: ${c.character_class} Gender: ${c.gender} </li>`)
-      
-      // playerCharacters.push(`<tr><td>${c.name}</td><td>${c.race}</td><td>${c.character_class}</td><td>${c.gender}</td></tr>`)
      return (`<tr data-id=${this.id}><td>${this.name}</td><td>${this.race}</td><td>${this.character_class}</td><td>${this.gender}</td>
      <td><a class="delete-character-link">Delete Char</a></td></tr>`)
-
-
-    // })
-    // playerCharacters.unshift(`<li>Player: ${this.name}</li>`)
-    // console.log(playerCharacters)
-    // return playerCharacters.join('')
   }
 
 }
