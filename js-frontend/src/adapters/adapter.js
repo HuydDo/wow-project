59c6647191bbd45fcdf5ba890e73282ebc71ed86
HuydDo <huydinhdo@yahoo.com>
2020-02-28 23:23:53 -0700

    Update adapter.js

--- js-frontend/src/adapters/adapter.js
+++ js-frontend/src/adapters/adapter.js
@@ -89,20 +89,16 @@ class Adapter {
   }
 
   nameCheck(str, value=0){
-    if (value == 1)
-      return `<div class='alert alert-warning alert-dismissible fade show'>
-              <button type='button' class='close' data-dismiss='alert'>&times;</button>
-              Can't find the player ${str}.  Please try again or create a new player.</div>`
-     if (value == 2){
-       return `<div class='alert alert-warning alert-dismissible fade show'>
-       <button type='button' class='close' data-dismiss='alert'>&times;</button>
-       ${str} </div>`
-     }
-
-      else
-      return `<div class='alert alert-warning alert-dismissible fade show'>
-              <button type='button' class='close' data-dismiss='alert'>&times;</button>
-              ${str} can't be empty</div>`
+    let btnStr =`<div class='alert alert-warning alert-dismissible fade show'>
+    <button type='button' class='close' data-dismiss='alert'>&times;</button>`
+    if (value == 1){
+      return btnStr += ` Can't find the player ${str}.  Please try again or create a new player.</div>`
+    }
+    if (value == 2){
+      return btnStr +=  `${str} </div>`
+    }
+    else {
+      return btnStr +=   `${str} can't be empty</div>`       
+    }
   }
-  
 }
